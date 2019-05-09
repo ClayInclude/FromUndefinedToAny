ef4b5e4fed38a6e93f4560aae8aa3768afe3c71b
Clay Include <includeclay@gmail.com>
2019-05-09 21:35:37 +0800

    ...

--- ProgrammingInC/chapter10/practice/practice06.c
+++ ProgrammingInC/chapter10/practice/practice06.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
-void removeString(/*char const *src, int start, int numbers*/);
+void testRemoveString(char *src, int start, int numbers);
 
 int main(void)
 {
-    removeString(/*"", 0, 0*/);
+    char text[] = "the wrong son";
+    testRemoveString(text, 4, 6);
 
     return 0;
 }
 
-void removeString(/*char const *src, int start, int numbers*/)
+void removeString(char *src, int start, int numbers)
 {
+    if (!numbers) return;
 
+    src += start;
+    char const *keep = src + numbers;
+
+    while (*keep != '\0')
+    {
+        *src++ = *keep++;
+    }
+
+    *src = '\0';
+}
+
+void testRemoveString(char *src, int start, int numbers)
+{
+    printf("%s remove %i char from %i: ", src, numbers, start);
+    removeString(src, start, numbers);
+    printf("%s\n", src);
 }
---
+++ ProgrammingInC/chapter10/practice/practice07.c
@@ -0,0 +1,6 @@
+/**
+ * practice07
+ * @author Clay
+ * @version 2019/5/9
+ */
+
