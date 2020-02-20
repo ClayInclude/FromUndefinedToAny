fb983f740e1f0ece2e564a2d9bd7fd81f1f593e1
Clay Include <includeclay@gmail.com>
2020-02-20 20:05:49 +0800

    ...

---
+++ ComputerSystemsAProgrammersPerspective/chapter01/example/example01.c
@@ -0,0 +1,14 @@
+/**
+ * example01
+ * @author Clay
+ * @date 2020/2/20
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+
+    return 0;
+}
--- commitall.bat
+++ commitall.bat
@@ -1,3 +1,11 @@
+@echo off
+
 git add .
 git commit -m "..."
 git push
+
+echo done!
+
+pause
+
+@echo on
