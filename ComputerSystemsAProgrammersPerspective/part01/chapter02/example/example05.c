ab7d811566b961e65d0138a415d39242db4d2b06
Clay Include <includeclay@gmail.com>
2020-03-07 21:41:56 +0800

    ...

--- ComputerSystemsAProgrammersPerspective/part01/chapter02/example/example05.c
+++ ComputerSystemsAProgrammersPerspective/part01/chapter02/example/example05.c
@@ -8,12 +8,19 @@
 
 void test1(unsigned int x, unsigned int y);
 
+void test2(int x, int y);
+
 int main(void)
 {
     test1(3000000000, 3000000000);
     test1(1, 3000000000);
     test1(1, 2);
     test1(3, 7);
+
+    test2(2100000000, 2100000000);
+    test2(-2100000000, -2100000000);
+    test2(1, -3);
+    test2(3, 999999999);
 
     return 0;
 }
@@ -25,9 +32,32 @@ int uAddOk(unsigned int x, unsigned int y)
     return s >= x && s >= y;
 }
 
+int tAddOk(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return x + y > 0;
+    }
+    else if (x < 0 && y < 0)
+    {
+        return x + y < 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
 void test1(unsigned int x, unsigned int y)
 {
     char const *prompt = uAddOk(x, y) ? "" : " not";
 
     printf("%u + %u is%s OK\n", x, y, prompt);
+}
+
+void test2(int x, int y)
+{
+    char const *prompt = tAddOk(x, y) ? "" : " not";
+
+    printf("%d + %d is%s OK\n", x, y, prompt);
 }
