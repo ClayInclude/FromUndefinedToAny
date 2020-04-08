5ce6dce8660915259f54b7afb3989082b79b4a7b
Clay Include <includeclay@gmail.com>
2020-04-08 19:01:43 +0800

    ...

--- ComputerSystemsAProgrammersPerspective/part01/chapter03/example/example01.c
+++ ComputerSystemsAProgrammersPerspective/part01/chapter03/example/example01.c
@@ -35,3 +35,23 @@ int64_t scale(int64_t x, int64_t y, int64_t z)
 {
     return x + 4 * y + 12 * z;
 }
+
+long int arith(long int x, long int y, long int z)
+{
+    long int t1 = x ^ y; // NOLINT(hicpp-signed-bitwise)
+    long int t2 = z * 48;
+    long int t3 = t1 & 0x0f0f0f0f; // NOLINT(hicpp-signed-bitwise)
+    long int t4 = t2 - t3;
+
+    return t4;
+}
+
+long int arith2(long int x, long int y, long int z)
+{
+    long int t1 = x | y; // NOLINT(hicpp-signed-bitwise)
+    long int t2 = t1 >> 3; // NOLINT(hicpp-signed-bitwise)
+    long int t3 = ~t2; // NOLINT(hicpp-signed-bitwise)
+    long int t4 = z - t3;
+
+    return t4;
+}
